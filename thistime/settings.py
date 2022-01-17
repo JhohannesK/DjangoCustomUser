f0f6fcc40b1d17fc835597b9ba222c18f20b2434
Domeh John <jdomeh77@gmail.com>
2022-01-17 13:34:49 +0000

    modified secret key portion

--- thistime/settings.py
+++ thistime/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+from django.core.management import utils
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m*_#))e%&pe%bps&_v_u45-klf0a=8qoi_uge(%cq1c(l_r7_^'
+SECRET_KEY = utils.get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
